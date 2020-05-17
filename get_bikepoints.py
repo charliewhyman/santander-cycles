98f9c2324ced10d10b4a5c8225a2205f06f2e2b2
charliewhyman <cwhyma@gmail.com>
2020-05-17 10:50:33 +0100

    Create get_bikepoints.py
    This script requests the current 'bikepoints' data from the Unified API,  showing names and locations of Santander Cycle docking stations.

---
+++ get_bikepoints.py
@@ -0,0 +1,17 @@
+import urllib.request
+import json
+import pandas as pd
+
+# Define bikepoint URL (from TfL Unified API)
+bikepoint_url = "https://api.tfl.gov.uk/BikePoint"
+
+# Request and open bikepoint url
+response = urllib.request.urlopen(bikepoint_url)
+bikepoints = json.loads(response.read())
+
+# Load json file into a data frame
+df = pd.read_json(bikepoint_url, orient='columns')
+
+# Keep id, name and lat/long fields
+df.drop(df.columns.difference(['id','commonName','lat','long']), 1, inplace=True)
+df.to_csv("out.csv",index=False)
